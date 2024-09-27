e39cb59f74c90c956129b5c5b62d23b552b426a0
AtharvaD10 <deshpandeatharva10@yahoo.com>
2024-09-27 17:41:54 +0530

    created login API

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,20 +1,63 @@
 const express = require("express");
+const { Error } = require("mongoose");
+const bcrypt = require('bcrypt');
+const validator = require("validator")
 const connectDB = require("./config/database");
 const User = require("./models/user");
-const { Error } = require("mongoose");
+const {validateSignUpData} = require("./utils/validation")
 
 const app = express();
 app.use(express.json())
 
+
+
 app.post("/signup",async (req,res)=>{
-  const user = new User(req.body);
     try{
-        await user.save();
-        res.send("User added sucessfully...");
-    }catch(err){
+            //validation of data
+             validateSignUpData(req);
+
+            //Encryption of Password
+            const {firstName, lastName, emailId, password} = req.body;
+            const hashPassword =  await bcrypt.hash(password,5);
+            
+            
+            //creating a new instance of user model
+            const user = new User({
+                firstName,
+                lastName,
+                emailId,
+                password:hashPassword
+            });
+              await user.save();
+              res.send("User added sucessfully...");
+        }catch(err){
         res.status(400).send("Error :"+err.message);
     }
 });
+
+app.post("/login",async(req,res)=>{
+    try{
+           const {emailId,password} = req.body;
+           if(!validator.isEmail(emailId)){
+            throw new Error("Enter Valid emailId");
+           }
+           const user = await User.findOne({emailId : emailId})
+           if(!user){
+            throw new Error("Invalid credentials")
+           }
+            const isPasswordValid  = await bcrypt.compare(password,user.password)
+
+           if(isPasswordValid){
+            res.send("Login sucessfully...")
+           }else{
+            throw new Error("Invalid credentials")
+           }
+           
+       }
+     catch(err){
+        res.status(400).send("Error :"+err.message)
+    }
+})
 
 app.get("/user",async (req,res)=>{
     const userEmail = req.body.emailId ;
