061137c4e96eb9fb5686ff9bf2f1dfb84c112650
AtharvaD10 <deshpandeatharva10@yahoo.com>
2024-10-07 17:55:11 +0530

    setup redux store

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Navbar from "./Navbar"
-import Body from "./Body"
-import Login from "./Login"
+import Navbar from "./components/Navbar"
+import Body from "./components/Body"
+import Login from "./components/Login"
+import { Provider } from "react-redux"
+import appStore from "./utils/appStore"
+import Feed from "./components/Feed"
 
 function App() {
 
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-            <Route path="/" element={<Body/>}>
-                  <Route path="/login" element={<Login/>}/>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+             <Route path="/" element={<Feed/>} />
+              <Route path="/login" element={<Login />} />
             </Route>
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,23 +1,33 @@
 import axios from 'axios';
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../utils/constants';
+
 
 const Login = () => {
 
     const [emailId, setEmailId] = useState("");
     const [password,setPassword] = useState("");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
 
     const handleLogin = async()=>{
     try { 
-            const res = await axios.post("http://localhost:7777/login",{
+            const res = await axios.post(BASE_URL + "/login",{
              emailId,
              password
           },
-          {withCredentials: true}
+          { withCredentials: true}
         );
+       
+        dispatch(addUser(res.data))
+        return navigate('/')
        }
         catch(err){
-            console.log(err);
-            
+            console.log(err);   
         }
     }
 
